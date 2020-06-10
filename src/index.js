68e6e9d7027a9c12c360b54e99383c76ac356018
Viktor <Vituna9@bk.ru>
2020-06-10 12:09:49 +0300

    Подключение React, рендеринг главной страницы приложения

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./components/app/app.jsx";
+
+const settings = {
+  MOVIE_TITLE: `The Grand Budapest Hotel`,
+  MOVIE_GENRE: `Drama`,
+  MOVIE_RELEASE_DATE: 2014,
+};
+
+ReactDOM.render(
+    <App
+      movieTitle={settings.MOVIE_TITLE}
+      movieGenre={settings.MOVIE_GENRE}
+      movieReleaseDate={settings.MOVIE_RELEASE_DATE}
+    />, document.querySelector(`#root`)
+);
