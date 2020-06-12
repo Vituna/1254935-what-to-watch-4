37adf12df20857a3c714c7cbdc6ea9c26736ab61
Viktor <Vituna9@bk.ru>
2020-06-12 14:41:28 +0300

    Подключает пакет prop-types, отдельный рендринг карточки фильма

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Main from "../main/main.jsx";
 
-// eslint-disable-next-line react/prop-types
-const App = ({movieTitle, movieGenre, movieReleaseDate}) => {
+const App = ({movieTitle, movieGenre, movieReleaseDate, movieListTitle}) => {
 
   return <Main
     movieTitle={movieTitle}
     movieGenre={movieGenre}
     movieReleaseDate={movieReleaseDate}
+    movieListTitle={movieListTitle}
+
   />;
+};
+
+App.propTypes = {
+  movieTitle: PropTypes.string.isRequired,
+  movieGenre: PropTypes.string.isRequired,
+  movieReleaseDate: PropTypes.number.isRequired,
+  movieListTitle: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const Settings = {
+  MOVIE_LIST_TITLE: [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`],
   MOVIE_TITLE: `The Grand Budapest Hotel`,
   MOVIE_GENRE: `Drama`,
   MOVIE_RELEASE_DATE: 2014,
@@ -11,6 +12,7 @@ const Settings = {
 
 ReactDOM.render(
     <App
+      movieListTitle={Settings.MOVIE_LIST_TITLE}
       movieTitle={Settings.MOVIE_TITLE}
       movieGenre={Settings.MOVIE_GENRE}
       movieReleaseDate={Settings.MOVIE_RELEASE_DATE}
